6721f712a4d7b36e71c24cb2a528f7d078b92a0c
OmKharade <133861833+OmKharade@users.noreply.github.com>
2024-07-15 22:14:18 +0530

    Overdoing tictactoe

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# odin-tictactoe
+# tictactoe
+
+<img src="assets/img1.png" height=400px>
+<img src="assets/img2.png" height=400px>
+
+## Overview
+Everyone builds this at some point in their career. Here is me walking down the path laid by those before me. If I keep walking, I might get where they are, get as cracked as some of the people I read about.
+
+This project solidified my understanding of Javascript functions and abstraction. Interesting colors, clean looking tictactoe. Completely overdoing a simple task.
+
+## Features
+
+- It works
+- It looks beautiful (i think so)
+- It's tictactoe
+
+## Live Demo
+
+Click [here](https://omkharade.github.io/odin-tictactoe/) to play
+
+## More Projects
+
+[This](https://github.com/OmKharade/odin-library) is what I tried next
+
+#### Visit my [GitHub Profile](https://github.com/OmKharade). Feedback is very appreciated !
+
--- script.js
+++ script.js
@@ -1,99 +1,221 @@
+const Player = (name, mark) => {
+  this.name = name;
+  this.mark = mark;
+
+  const getName = () => name;
+
+  const getMark = () => mark;
+
+  return { getMark, getName };
+};
+
 const GameBoard = () => {
-    const board = new Array(9);
-  
-    const setField = (index, mark) => {
-      if (index > board.length) return;
-      board[index] = mark;
-    };
-  
-    const getField = (index) => {
-      if (index > board.length) return;
-      return board[index];
-    };
-  
-    const clearBoard = () => {
-      for (let index = 0; index < 9; index++) {
-        board[index] = undefined;
-      }
-    };
-  
-    const printBoard = () => {
-      let string = "";
-      for (let index = 0; index < 9; index++) {
-        if (index % 3 == 0) {
-          string += "\n" + (board[index] ? board[index] : "*") + " ";
-        } else {
-          string += (board[index] ? board[index] : "*") + " ";
-        }
-      }
-      console.log(string);
-    };
-  
-    return { clearBoard, getField, printBoard, setField };
+  const _board = new Array(9);
+
+  const setField = (index, mark) => {
+    if (index > _board.length) return;
+    _board[index] = mark;
   };
 
-  
+  const getField = (index) => {
+    if (index > _board.length) return;
+    return _board[index];
+  };
+
+  const clearBoard = () => {
+    for (let index = 0; index < 9; index++) {
+      _board[index] = undefined;
+    }
+  };
+
+  return { clearBoard, getField, setField };
+};
+
 const gameController = (() => {
-    const playerX = { name: "X Player", mark: "X" };
-    const playerO = { name: "O Player", mark: "O" };
-    const gameBoard = GameBoard();
+  const playerX = Player("cross", "X");
+  const playerO = Player("circle", "O");
+  const gameBoard = GameBoard();
 
-    let activePlayer = playerX;
+  let round = 1;
+  let isGameOver = false;
 
-    const switchPlayerTurn = () =>
-        (activePlayer = activePlayer === playerX ? playerO : playerX);
+  const playRound = (index) => {
+    gameBoard.setField(index, getCurrentPlayer().getMark());
 
-    const checkForWin = (index) => {
-        const winCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        ];
+    if (checkForWin(index)) {
+      isGameOver = true;
+      displayController.displayWinner(
+        getCurrentPlayer().getMark(),
+        winningCombination()
+      );
+      return;
+    }
+    if (round === 9) {
+      isGameOver = true;
+      displayController.displayWinner("D");
+      return;
+    }
 
-        return winCombinations
-        .filter((combination) => combination.includes(index))
-        .some((possibleCombination) =>
-            possibleCombination.every(
-            (fieldIndex) => gameBoard.getField(fieldIndex) === activePlayer.mark
-            )
+    ++round;
+    displayController.displayMessage(
+      `<span>${getCurrentPlayer().getMark()}</span><span>TURN</span>`
+    );
+  };
+
+  const getCurrentPlayer = () => (round % 2 === 1 ? playerX : playerO);
+
+  const winningCombination = () => {
+    const winCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    let result;
+    winCombinations.forEach((combination) => {
+      if (
+        gameBoard.getField(combination[0]) ===
+          gameBoard.getField(combination[1]) &&
+        gameBoard.getField(combination[1]) ===
+          gameBoard.getField(combination[2])
+      ) {
+        result = combination;
+      }
+    });
+    return result;
+  };
+
+  const checkForWin = (index) => {
+    const winCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    return winCombinations
+      .filter((combination) => combination.includes(index))
+      .some((possibleCombination) =>
+        possibleCombination.every(
+          (fieldIndex) =>
+            gameBoard.getField(fieldIndex) === getCurrentPlayer().getMark()
+        )
+      );
+  };
+
+  const getIsGameOver = () => isGameOver;
+
+  const resetGame = () => {
+    gameBoard.clearBoard();
+    isGameOver = false;
+    round = 1;
+  };
+
+  return {
+    getCurrentPlayer,
+    getIsGameOver,
+    playRound,
+    resetGame,
+  };
+})();
+
+const displayController = (() => {
+  const fieldElements = document.querySelectorAll(".board-field"),
+    scoreDisplayElements = document.querySelectorAll(".display-score"),
+    restartButton = document.querySelector(".btn-restart"),
+    messageDisplayElement = document.querySelector(".message-display");
+
+  let circleWinCount = 0,
+    crossWinCount = 0,
+    drawGameCount = 0;
+
+  fieldElements.forEach((element) =>
+    element.addEventListener("click", (event) => {
+      const fieldIndex = parseInt(event.target.dataset.index);
+      if (gameController.getIsGameOver() || event.target.innerHTML !== "")
+        return;
+
+      updateBoard(fieldIndex);
+      gameController.playRound(fieldIndex);
+    })
+  );
+
+  const updateBoard = (index) => {
+    const name = gameController.getCurrentPlayer().getName();
+    fieldElements[
+      index
+    ].innerHTML = `<img src='./assets/${name}.svg' alt='${name}' />`;
+    fieldElements[index].classList.add("disabled");
+  };
+
+  const displayMessage = (htmlContent) => {
+    messageDisplayElement.innerHTML = htmlContent;
+  };
+
+  const displayWinner = (winner, combination) => {
+    switch (winner) {
+      case "D":
+        displayMessage(`<span>DRAW</span>`);
+        updateScore(winner);
+        messageDisplayElement.classList.add("draw");
+        return;
+      case "X":
+        displayMessage(`<span>X</span><span>WON</span>`);
+        combination.forEach((index) =>
+          fieldElements[index].classList.add(`winner-x`)
         );
-    };
-
-    const checkForDraw = () => {
-        for (let index = 0; index < 9; index++) {
-        if (gameBoard.getField(index) === undefined) {
-            return false;
-        }
-        }
-        return true;
-    };
-
-    const playRound = (index) => {
-        gameBoard.setField(index, activePlayer.mark);
-        gameBoard.printBoard();
-        if (checkForWin(index)) {
-        console.log(`${activePlayer.name} WON`);
+        messageDisplayElement.classList.add("winner-x");
+        updateScore(winner);
         return;
-        }
-        if (checkForDraw()) {
-        console.log("It's a tie!");
+      case "O":
+        displayMessage(`<span>O</span><span>WON</span>`);
+        combination.forEach((index) =>
+          fieldElements[index].classList.add(`winner-o`)
+        );
+        messageDisplayElement.classList.add("winner-o");
+        updateScore(winner);
         return;
-        }
-        switchPlayerTurn();
-        consoleGame();
-    };
+      default:
+        console.error("Invalid Winner: " + winner);
+        return;
+    }
+  };
 
-    const consoleGame = () => {
-        console.log(`${activePlayer.name}'s Turn`);
-        const fieldIndex = parseInt(prompt("Input the Index:"));
-        playRound(fieldIndex);
-    };
+  const updateScore = (winner) => {
+    switch (winner) {
+      case "D":
+        scoreDisplayElements[1].textContent = ++drawGameCount;
+        return;
+      case "X":
+        scoreDisplayElements[0].textContent = ++crossWinCount;
+        return;
+      case "O":
+        scoreDisplayElements[2].textContent = ++circleWinCount;
+        return;
+      default:
+        console.error("Invalid Winner: " + winner);
+        return;
+    }
+  };
 
-    console.log("Game Start");
-    consoleGame();
+  restartButton.addEventListener("click", () => {
+    gameController.resetGame();
+    fieldElements.forEach((element) => {
+      element.className = "board-field";
+      element.innerHTML = "";
+    });
+    displayMessage("<span>X</span><span>TURN</span>");
+    messageDisplayElement.className = "message-display";
+  });
+
+  return { displayMessage, displayWinner };
 })();
